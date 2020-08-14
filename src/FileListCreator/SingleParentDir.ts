674a425b42d4c8a41584be112ec9418d1aa64229
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-08-14 20:13:06 +0530

    using glob

--- src/FileListCreator/SingleParentDir.ts
+++ src/FileListCreator/SingleParentDir.ts
@@ -1,6 +1,6 @@
-import * as fs from "fs";
 import * as path from "path";
 import * as core from "@actions/core";
+import * as glob from "glob";
 import { FileList } from "./FileListCreator";
 
 export default class SingleParentDir implements FileList {
@@ -10,48 +10,26 @@ export default class SingleParentDir implements FileList {
         this.filePathRegex = filePathRegex;
     }
 
+    /**
+     * Get list of files to execute in lexicographical order from the given filePathRegex
+     * Supported filePathRegex are: 
+     *      <file>.sql
+     *      <folder>/<regex>.sql
+     *      <folder1>/<folder2>/<folder3>/<regex>.sql
+     */
     getFileList(): any {
         core.debug(`Getting list of files to execute`);
         const basedir = process.env.GITHUB_WORKSPACE;
         if (!basedir) {
             throw new Error("GITHUB_WORKSPACE env variable is empty");
         };
-        const targetDirectory = this.getTargetDirectory(basedir, this.filePathRegex);
-        const filesInTargetDir = fs.readdirSync(targetDirectory);
-        core.debug(`File list for matching against regex: ${filesInTargetDir}`);
+        let listOfMatchedFiles = glob.sync(this.filePathRegex, {});
+        core.debug(`Matching list of files: ${listOfMatchedFiles}`);
 
-        const fileNameFromRegex: RegExp = new RegExp(this.getFileNameFromRegex(this.filePathRegex));
-        core.debug(`Regex to match file names in targetDirectory: ${fileNameFromRegex}`);
-
-        const listOfMatchedFiles: string[] = [];
-        let absoluteFilePath;
-        filesInTargetDir.forEach( (file) => {
-            absoluteFilePath = path.join(targetDirectory, file);
-            if (fs.lstatSync(absoluteFilePath).isFile() && fileNameFromRegex.test(file)) {
-                core.debug(`Matching files found: ${file}`);
-                listOfMatchedFiles.push(absoluteFilePath);
-            }
-        });
         listOfMatchedFiles.sort();
+        listOfMatchedFiles = listOfMatchedFiles.map( (fileName) => path.join(basedir, fileName) );
         console.log("List of files to be executed in order: " + listOfMatchedFiles);
         return listOfMatchedFiles;
-    }
-
-    private getTargetDirectory(basedir:string, sqlFilesPathRegex: string) {
-        const folderPathFromRegex = sqlFilesPathRegex.slice(0, this.getLastIndex(sqlFilesPathRegex));
-        core.debug(`folder path from ${sqlFilesPathRegex} is ${folderPathFromRegex}`);
-        
-        const targetDirectory = path.join(basedir, folderPathFromRegex);
-        core.debug(`TargetDirectory: ${targetDirectory}`);
-        return targetDirectory;
-    }
-
-    private getLastIndex(sqlFilesPathRegex: string) {
-        return sqlFilesPathRegex.lastIndexOf("/") + 1;
-    }
-
-    private getFileNameFromRegex(sqlFilesPathRegex: string) {
-        return sqlFilesPathRegex.slice(this.getLastIndex(sqlFilesPathRegex), sqlFilesPathRegex.length);
     }
 
 }
