4ce460a6dc4da5c4923b699bb7615254b409acf1
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-08-16 23:39:29 +0530

    adding unit tests

---
+++ __tests__/FileListCreator/FileListCreator.test.ts
@@ -0,0 +1,34 @@
+import { FileListCreator } from "../../src/FileListCreator/FileListCreator";
+import FileConstants from "../../src/Constants/FileConstants";
+
+const singleFile = "1.sql";
+const multipleFiles = "*.sql";
+const filesInsideFolder = "x/y/*_db.sql";
+const regexToFail = "x/*/";
+
+describe('Testing fileListCreator', () => {
+    let getFileListSpy;
+    
+    beforeAll( () => {
+        getFileListSpy = jest.spyOn(FileListCreator, 'getFileList');
+        getFileListSpy.mockImplementation((filePathRegex) => {
+            let matchesFilePath = false;
+            if (FileConstants.singleParentDirRegex.test(filePathRegex)) {
+                matchesFilePath = true;
+            }
+            return matchesFilePath;
+        });
+    });
+
+    test('getFileList should pass', () => {
+        expect(getFileListSpy(singleFile)).toBeTruthy;
+        expect(getFileListSpy(multipleFiles)).toBeTruthy;
+        expect(getFileListSpy(filesInsideFolder)).toBeTruthy;
+    });
+
+    test('getFileList should fail', () => {
+        expect(getFileListSpy(regexToFail)).toBeFalsy;
+    });
+
+});
+
---
+++ __tests__/PsqlFilesExecutor.test.ts
@@ -0,0 +1,27 @@
+import PsqlFilesExecutor from "../src/PsqlFilesExecutor";
+
+jest.mock('../src/Utils/PsqlUtils/PsqlToolRunner');
+jest.mock('../src/FileListCreator/FileListCreator');
+
+let psqlFilesExecutor: PsqlFilesExecutor;
+
+beforeAll(() => {
+    psqlFilesExecutor = PsqlFilesExecutor.getPsqlFilesExecutor('connString','filePath','args');
+});
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+describe('Testing PsqlFilesExecutor', () => {
+    let executeSpy;
+    beforeEach(() => {
+        executeSpy = jest.spyOn(psqlFilesExecutor, 'execute');
+    });
+
+    test('execute should pass', async () => {
+        executeSpy.mockImplementationOnce(() => Promise.resolve());
+        await psqlFilesExecutor.execute();
+        expect(executeSpy).toHaveBeenCalled();
+    });
+});
