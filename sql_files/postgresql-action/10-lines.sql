1aec3883bc62421d96eb6fc3d3319f2e189a3cb1
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-08-19 20:10:27 +0530

    adding benchmarking files

---
+++ sql_files/postgresql-action/10-lines.sql
@@ -0,0 +1,16 @@
+\c aktest;
+\dt
+delete from temp;
+select * from temp;
+insert into temp values(100,'foo');
+insert into temp values(101,'foo');
+insert into temp values(102,'foo');
+insert into temp values(103,'foo');
+insert into temp values(104,'foo');
+insert into temp values(105,'foo');
+insert into temp values(106,'foo');
+insert into temp values(107,'foo');
+insert into temp values(108,'foo');
+insert into temp values(109,'foo');
+insert into temp values(110,'foo');
+select * from temp;
