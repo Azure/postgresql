c039b614282719495d5726393e59742a61b4ec63
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-08-21 12:26:32 +0530

    added comments

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,5 @@
 export class FileConstants {
+    // regex checks that string should end with .sql and if folderPath is present, * should not be included in folderPath
     static readonly singleParentDirRegex = /^((?!\*\/).)*(\.sql)$/g;
 }
 
@@ -8,6 +9,8 @@ export class FirewallConstants {
 
 export class PsqlConstants {
     static readonly SELECT_1 = "SELECT 1";
+    // host, port, dbname, user, password must be present in connection string in any order.
     static readonly connectionStringTestRegex = /^(?=.*host(\s)*=(\s)*.+)(?=.*port(\s)*=(\s)*.+)(?=.*dbname(\s)*=(\s)*.+)(?=.*user(\s)*=(\s)*.+)(?=.*password(\s)*=(\s)*.+).+/g;
+    // extracting password from connection string
     static readonly extractPasswordRegex = /(?<key>password)\s*=\s*(?<val>[^\s]*)/g;
 }
